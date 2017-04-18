b3c46768c6fc2a733e201e68c54fb684dab8ccc7
setrar <setrar@valiha.com>
2017-04-18 12:18:23 -0400

    Init3

--- README.md
+++ README.md
@@ -2,5 +2,25 @@
 Running SoundManager2 with Angular CLI
 
 ```
-$ ng new sm2
+$ ng new frontend --style=scss
+$ cd frontend
+```
+
+install soundmanager2
+```
+$ npm install soundmanager2 --save
+```
+
+
+Edit .angular-cli.json  i
+```
+      "scripts": [
+        "../node_modules/soundmanager2/script/soundmanager2-jsmin.js"
+      ],
+```
+
+copy all flash files to assets folder
+```
+$ mkdir src/assets/sm2
+$ cp -R node_modules/soundmanager2/swf src/assets/sm2/
 ```
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare const soundManager: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit() {
+    soundManager.setup({
+      url: '/assets/sm2/swf/',
+      preferFlash: true,
+      debugFlash: true,
+      flashVersion: 9,
+      useFlashBlock: true,
+      onready: function() {
+        const sound = soundManager.createSound({
+          id: 'soundid',
+          serverURL: 'rtmp://media.crave.fm:1935/vod/',
+          url: [
+            'mp3:TIAr0000000196Al0000000001So0000006243.mp3',
+            'mp3:TIAr0000000196Al0000000001So0000006243.mp3'
+          ],
+          onconnect: function(bConnect) {
+            // this.connected can also be used
+            soundManager._writeDebug(this.id + ' connected: ' + (bConnect ? 'true' : 'false' ));
+          },
+          duration: 300000, // example for a 5 minutes song
+          autoLoad: true,
+          autoPlay: false,
+          onload: function() {
+            console.log('DURATION: ', this.duration);
+          },
+          whileplaying: function() {
+            console.log(this.duration);
+          },
+          volume: 100
+        });
+        sound.play();
+      },
+      ontimeout: function() {
+        // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+      }
+    });
+  }
 }
